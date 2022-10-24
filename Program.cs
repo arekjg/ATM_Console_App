30d9384b2e04a709dfbaf2a8f31b93cc2ef5bbbf
arekjg <arekjg@gmail.com>
2022-10-24 20:09:14 +0200

    fixed getters and setters

--- Program.cs
+++ Program.cs
@@ -5,71 +5,85 @@ using System.IO;
 using CsvHelper;
 using System.Globalization;
 
-// app based on tutorial from: https://www.youtube.com/watch?v=qBI7Qnz9Zho
-
 public class cardHolder
 {
-    public String cardNum { get; set; }    // card number
-    public int pin { get; set; }           // pin number
-    public String firstName { get; set; }  // first name
-    public String lastName { get; set; }   // last name
-    public double balance { get; set; }    // balance
-
-    //public cardHolder(string cardNum, int pin, string firstName, string lastName, double balance)
-    //{
-    //    this.cardNum = cardNum;
-    //    this.pin = pin;
-    //    this.firstName = firstName;
-    //    this.lastName = lastName;
-    //    this.balance = balance;
-    //}
-
-    // getters
-    public String getNum()
+    public cardHolder(string cardNum, int pin, string firstName, string lastName, double balance)
     {
-        return cardNum;
+        CardNum = cardNum;
+        Pin = pin;
+        FirstName = firstName;
+        LastName = lastName;
+        Balance = balance;
     }
 
-    public int getPin()
+    private String cardNum;    // card number
+
+    public String CardNum
     {
-        return pin;
+        get
+        {
+            return cardNum;
+        }
+        set
+        {
+            cardNum = value;
+        }
     }
 
-    public String getFirstName()
+    private int pin;           // pin number
+
+    public int Pin
     {
-        return firstName;
-    }
-    
-    public String getLastName()
-    {
-        return lastName;
+        get
+        {
+            return pin;
+        }
+        set
+        {
+            pin = value;
+        }
     }
 
-    public double getBalance()
+    private String firstName;  // first name
+
+    public String FirstName
     {
-        return balance;
+        get
+        {
+            return firstName;
+        }
+        set
+        {
+            firstName = value;
+        }
     }
 
-    // setters
-    public void setNum(String newCardNum)
+    private String lastName;   // last name
+
+    public String LastName
     {
-        cardNum = newCardNum;
+        get
+        {
+            return lastName;
+        }
+        set
+        {
+            lastName = value;
+        }
     }
-    public void setPin(int newPin)
+
+    private double balance;    // balance
+
+    public double Balance
     {
-        pin = newPin;
-    }
-    public void setFirstName(String newFirstName)
-    {
-        firstName = newFirstName;
-    }
-    public void setLastName(String newLastName)
-    {
-        lastName = newLastName;
-    }
-    public void setBalance(double newBalance)
-    {
-        balance = newBalance;
+        get
+        {
+            return balance;
+        }
+        set
+        {
+            balance = value;
+        }
     }
 
     public static void Main(String[] args)
@@ -88,8 +102,8 @@ public class cardHolder
             Console.WriteLine("How much money would you like to deposit: ");
 
             double deposit = Double.Parse(Console.ReadLine());
-            currentUser.setBalance(currentUser.getBalance() + deposit);
-            Console.WriteLine("Thank you for your money. Your new balance is: " + currentUser.getBalance());
+            currentUser.Balance = currentUser.Balance + deposit;
+            Console.WriteLine("Thank you for your money. Your new balance is: " + currentUser.Balance);
         }
 
         void withdraw(cardHolder currentUser)
@@ -98,36 +112,31 @@ public class cardHolder
             
             double withdrawal = Double.Parse(Console.ReadLine());
             // Check if the user has enough money
-            if (currentUser.getBalance() < withdrawal)
+            if (currentUser.Balance < withdrawal)
             {
                 Console.WriteLine("Insufficient balance :(");
             }
             else
             {
-                currentUser.setBalance(currentUser.getBalance() - withdrawal);
+                currentUser.Balance = currentUser.Balance - withdrawal;
                 Console.WriteLine("You're good to go! Thank you :)");
             }
         }
 
         void balance(cardHolder currentUser)
         {
-            Console.WriteLine("Current balance: " + currentUser.getBalance());
+            Console.WriteLine("Current balance: " + currentUser.Balance);
         }
 
-        var cardHolders = new List<cardHolder>()
-        {
-            new cardHolder{ cardNum = "4532772818527395", pin = 1234, firstName = "John", lastName = "Griffith", balance = 150.31 },
-            new cardHolder{ cardNum = "8645218942348435", pin = 4321, firstName = "Ashley", lastName = "Jones", balance = 321.13 },
-            new cardHolder{ cardNum = "8726646943484746", pin = 9999, firstName = "Frida", lastName = "Dickerson", balance = 105.59 },
-            new cardHolder{ cardNum = "1388546976325648", pin = 2468, firstName = "Muneeb", lastName = "Harding", balance = 851.84 },
-            new cardHolder{ cardNum = "5135893462349531", pin = 4826, firstName = "Dawn", lastName = "Smith", balance = 54.27 }
-        };
-        // database
-        //cardHolders.Add(new cardHolder("4532772818527395", 1234, "John", "Griffith", 150.31));
-        //cardHolders.Add(new cardHolder("8645218942348435", 4321, "Ashley", "Jones", 321.13));
-        //cardHolders.Add(new cardHolder("8726646943484746", 9999, "Frida", "Dickerson", 105.59));
-        //cardHolders.Add(new cardHolder("1388546976325648", 2468, "Muneeb", "Harding", 851.84));
-        //cardHolders.Add(new cardHolder("5135893462349531", 4826, "Dawn", "Smith", 54.27));
+        // List of card holders
+        var cardHolders = new List<cardHolder>();
+
+        // Database
+        cardHolders.Add(new cardHolder("4532772818527395", 1234, "John", "Griffith", 150.31));
+        cardHolders.Add(new cardHolder("8645218942348435", 4321, "Ashley", "Jones", 321.13));
+        cardHolders.Add(new cardHolder("8726646943484746", 9999, "Frida", "Dickerson", 105.59));
+        cardHolders.Add(new cardHolder("1388546976325648", 2468, "Muneeb", "Harding", 851.84));
+        cardHolders.Add(new cardHolder("5135893462349531", 4826, "Dawn", "Smith", 54.27));
 
         // CSV WRITER
         using (var writer = new StreamWriter("filePersons.csv"))
@@ -135,8 +144,6 @@ public class cardHolder
         {
             csv.WriteRecords(cardHolders);
         }
-
-
 
         // Prompt user
         Console.WriteLine("Welcome to SimpleATM");
@@ -166,13 +173,13 @@ public class cardHolder
             {
                 userPin = int.Parse(Console.ReadLine());
                 // Check against database
-                if (currentUser.getPin() == userPin) { break; }
+                if (currentUser.Pin == userPin) { break; }
                 else { Console.WriteLine("Incorrect pin. Please try again"); }
             }
             catch { Console.WriteLine("Incorrect pin. Please try again"); }
         }
 
-        Console.WriteLine("Welcome " + currentUser.getFirstName() + " :)");
+        Console.WriteLine("Welcome " + currentUser.FirstName + " :)");
         int option = 0;
         do
         {
